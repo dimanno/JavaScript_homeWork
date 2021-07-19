02d7ad9f8500e128ee4fef2ac54d3d3e2f6f218b
dimanno <dimanno.dev@gmail.com>
2021-07-19 10:35:42 +0300

    start classWork4

---
+++ classWork4/script.js
@@ -0,0 +1,25 @@
+// -створити класс попелюшка з полями ім'я, вік, розмір ноги
+
+class Popel
+
+
+
+// -створити функцію конструктор попелюшка з полями ім'я, вік, розмір ноги
+
+// function Popel(name, age, size) {
+//     this.name = name;
+//     this.age = age;
+//     this.size = size;
+// }
+
+
+
+// let girls = [
+//     new Popel('suzi', 18, 39);
+//     new Popel('lusi', 28, 40);
+//     new Popel('suzi', 18, 39);
+//     new Popel('lusi', 28, 40);
+//     new Popel('suzi', 18, 39);
+//     new Popel('lusi', 28, 40);
+//
+//     ]
