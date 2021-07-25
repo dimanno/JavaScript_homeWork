bf2c2e47c50b00d99ec3431771ee4bb08297b3c5
Dmytro Petskovych <dimanno.dev@gmail.com>
2021-07-25 23:51:49 +0300

    save classWork5.2

--- homeWork 5.2/getProduct.js
+++ homeWork 5.2/getProduct.js
@@ -21,7 +21,7 @@
 
      // Оптимізований варіант дістати інпути
 
-const = {productName, quantityProduct, price, pictures} = document.forms.getProduct;
+const {productName, quantityProduct, price, pictures} = document.forms.getProduct;
 // дістаємо кнопку
 const btn = document.getElementById('btn');
 // Для ключа у localstorage присвоюємо змінну
@@ -30,7 +30,7 @@ const key = 'key';
 // кнопці присвоюємо подію при якій будуть зберігатись значення інпутів
 // Для цього створимо функцію, яка буде зберігати значення введенні в інпути у localstorage
 
-const save = (productName, quantityProduct, price, pictures) = > {
+const save = (productName, quantityProduct, price, pictures) => {
      const store = JSON.parse(localStorage.getItem(key)) || [];
     // створили змінну store яка дорівнює пустому масиву у який ми будемо пушити значення з інпутів
     //  збережені у локал сторідж, або вже існуючим даним (ключам) у локал сторедж, які дістаємо і парсаємо через
@@ -38,7 +38,7 @@ const save = (productName, quantityProduct, price, pictures) = > {
      store.push({id:Math.random(), productName, quantityProduct, price, pictures})
     // Запушили введені обєкти інпутів, які берігаються у локал сторідж + рандомну id
     localStorage.setItem(key, JSON.stringify(store))
-    Зберігаємо дані у localStorage, який приймає два аргументи - ключ та значення (value)
+    // Зберігаємо дані у localStorage, який приймає два аргументи - ключ та значення (value)
 }
 
 btn.onclick = () => {
--- homeWork 5.2/productList.js
+++ homeWork 5.2/productList.js
@@ -3,6 +3,38 @@ const  key = 'key';
 
 const showlist = () => {
     if (!store.length) {
-
+        const empty = document.createElement('h3');
+        empty.innerText = 'list is empty';
+        divList.appendChild(empty)
     }
+const battotAll = document.createElement('button');
+for (const itemOfStore of store) {
+    const productbox = document.createElement('div');
+    const name = document.createElement('div');
+    const productImg = document.createElement('img');
+    const quantity = document.createElement('div');
+    const price = document.createElement('div');
+    const butoon = document.createElement('button');
+        name.innerText = 'Name: ${item.name'
+        productImg.src = item.img
+        price.innerText = 'Price; ${item.prace'
+        button.innerText = 'Delete'
+        buttonAll.innerText = 'DeleteAll'
+        butoon.onclick = () => {
+            deleteitemOfStore(item.id)
+        }
+    productbox.append(name, productImg, price, butoon)
+    divList.appendChild(productbox)
+}
+buttonAll.onclick = () => {
+    deleteAll()
+}
+divList.appendChild(battotAll);
+}
+
+const deleteIOtem = (id) => {
+    store = store.filter(value => value.id === id)
+    localStorage.setItem(key, JSON.stringify(store))
+    divList.innerHTML = ''
+    showlist
 }
