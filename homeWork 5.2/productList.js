cbf89a166d3bb9aa78eb11d650cd502b6967526f
Dmytro Petskovych <dimanno.dev@gmail.com>
2021-07-26 09:43:09 +0300

    save classWork5.2

--- homeWork 5.2/productList.js
+++ homeWork 5.2/productList.js
@@ -1,5 +1,6 @@
 const divList = document.getElementById('list');
 const  key = 'key';
+const store = JSON.parse(localStorage.getItem(key)) || [];
 
 const showlist = () => {
     if (!store.length) {
@@ -7,34 +8,43 @@ const showlist = () => {
         empty.innerText = 'list is empty';
         divList.appendChild(empty)
     }
-const battotAll = document.createElement('button');
+const buttonAll = document.createElement('button');
 for (const itemOfStore of store) {
     const productbox = document.createElement('div');
-    const name = document.createElement('div');
+    const nameL = document.createElement('div');
     const productImg = document.createElement('img');
-    const quantity = document.createElement('div');
-    const price = document.createElement('div');
-    const butoon = document.createElement('button');
-        name.innerText = 'Name: ${item.name'
-        productImg.src = item.img
-        price.innerText = 'Price; ${item.prace'
+    const quantityL = document.createElement('div');
+    const priceL = document.createElement('div');
+    const button = document.createElement('button');
+        nameL.innerText = "Name: ${itemOfStore.nameL}"
+        productImg.src = itemOfStore.img
+        priceL.innerText = 'Price: ${itemOfStore.priceL}'
+        quantityL.innerText = 'quantity: ${itemOfStore.quantityL}'
         button.innerText = 'Delete'
         buttonAll.innerText = 'DeleteAll'
-        butoon.onclick = () => {
+        button.onclick = () => {
             deleteitemOfStore(item.id)
         }
-    productbox.append(name, productImg, price, butoon)
+    productbox.append(nameL, productImg, priceL, button)
     divList.appendChild(productbox)
 }
 buttonAll.onclick = () => {
     deleteAll()
 }
-divList.appendChild(battotAll);
+divList.appendChild(buttonAll);
 }
 
 const deleteIOtem = (id) => {
     store = store.filter(value => value.id === id)
     localStorage.setItem(key, JSON.stringify(store))
     divList.innerHTML = ''
-    showlist
+    showlist();
 }
+
+const deleteAll = () => {
+    localStorage.clear()
+    showlist();
+    divList.innerHTML = 'List is empty';
+}
+
+showlist()
